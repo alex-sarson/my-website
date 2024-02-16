024c440e336f064c14446e7731877ac4e735a91a
notdospek <alex.luke.sarson@gmail.com>
2024-02-16 20:48:14 +0000

    added react icons to socials component

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -51,23 +51,4 @@ const HeaderContainer = styled.div`
   }
 `;
 
-// const Tile = styled.div`
-//   background-color: var(--surface-container);
-//   border-radius: var(--tile-border-radius);
-
-//   .tile-top{
-//     width: 100%;
-//     height: 200px;
-//     background-color: pink;
-//     border-radius: var(--tile-border-radius);
-//     overflow: hidden;
-
-//     img {
-//       height: 100%;
-//       width: 100%;
-//       object-fit: cover;
-//     }
-//   }
-// `
-
 export default AboutSection;
